d71370766da4486ee7a4e2360adc69e8482fb362
unknown <joaocarlos.roche@globo.com>
2021-02-11 21:23:39 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/method-signature-style */
 import { AddAccountModel } from '../../domain/usecases/add-account'
 import { AccountModel } from '../../domain/models/account'
 
 export interface AddAccountRepository {
-  add: (accountData: AddAccountModel) => Promise<AccountModel>
+  add (accountData: AddAccountModel): Promise<AccountModel>
 }
