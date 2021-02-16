b9f6353f09f54faebab45088b9c8b23345eba533
unknown <joaocarlos.roche@globo.com>
2021-02-16 19:21:12 -0300

    test: add tests to CompareFieldsValidations

--- src/presentation/helpers/validators/requerid-field-validation.spec.ts
+++ src/presentation/helpers/validators/requerid-field-validation.spec.ts
@@ -2,19 +2,19 @@ import { MissingParamError } from '../../errors'
 import { RequeridFieldValidation } from './requerid-field-validation'
 
 const makeSut = (): RequeridFieldValidation => {
-  return new RequeridFieldValidation('any_field')
+  return new RequeridFieldValidation('field')
 }
 
 describe('RequeridField Validation', () => {
   test('should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new MissingParamError('any_field'))
+    expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('should not return if validation succeeds', () => {
     const sut = makeSut()
     const error = sut.validate({ field: 'any_name' })
-    expect(error).toEqual(new MissingParamError('any_field'))
+    expect(error).toBeFalsy()
   })
 })
