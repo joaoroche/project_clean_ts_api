715a615cb84d92a92eb9b763856c2a5d5fca86cb
unknown <joaocarlos.roche@globo.com>
2021-02-16 18:59:50 -0300

    test: ensure requeridFiledValidation returns a missingParamError if validation fails

---
+++ src/presentation/helpers/validators/requerid-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { MissingParamError } from '../../errors'
+import { RequeridFieldValidation } from './requerid-field-validation'
+
+describe('RequeridField Validation', () => {
+  test('should return a MissingParamError if validation fails', () => {
+    const sut = new RequeridFieldValidation('any_field')
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+})
