03c46d7cb4037c980ad3ae49c4ec3f53c6b65a2e
unknown <joaocarlos.roche@globo.com>
2021-02-17 22:29:04 -0300

    refactor: move validation to protocols folder

--- src/presentation/helpers/validators/requerid-field-validation.ts
+++ src/presentation/helpers/validators/requerid-field-validation.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from '../../errors'
-import { Validation } from './validation'
+import { Validation } from '../../protocols/validation'
 
 export class RequeridFieldValidation implements Validation {
   private readonly fildName: string
