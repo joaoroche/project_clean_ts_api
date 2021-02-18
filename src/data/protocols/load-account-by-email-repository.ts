9a2e1cba44f63811cbfc3884746437d87e08a05f
unknown <joaocarlos.roche@globo.com>
2021-02-18 19:44:12 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/method-signature-style */
+import { AccountModel } from '../../domain/models/account'
+
+export interface LoadAccountByEmailRepository {
+  load (email: string): Promise<AccountModel>
+}
