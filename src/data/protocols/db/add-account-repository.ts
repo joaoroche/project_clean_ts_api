852d9de3ef4990338fbdd2ab7feb9328a19f736d
unknown <joaocarlos.roche@globo.com>
2021-02-18 20:03:53 -0300

    refactor: move data layer interfaces to folders

--- src/data/protocols/db/add-account-repository.ts
+++ src/data/protocols/db/add-account-repository.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/method-signature-style */
-import { AddAccountModel } from '../../domain/usecases/add-account'
-import { AccountModel } from '../../domain/models/account'
+import { AddAccountModel } from '../../../domain/usecases/add-account'
+import { AccountModel } from '../../../domain/models/account'
 
 export interface AddAccountRepository {
   add (accountData: AddAccountModel): Promise<AccountModel>
