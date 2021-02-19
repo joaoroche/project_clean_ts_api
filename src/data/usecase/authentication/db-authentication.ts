798469e6ee83461b900e3d3fd925aae04d6a4543
unknown <joaocarlos.roche@globo.com>
2021-02-18 21:00:50 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecase/authentication/db-authentication.ts
+++ src/data/usecase/authentication/db-authentication.ts
@@ -2,16 +2,19 @@ import { AuthenticationModel, Authentication } from '../../../domain/usecases/au
 import { LoadAccountByEmailRepository } from '../../../data/protocols/db/load-account-by-email-repository'
 import { HashComparer } from '../../protocols/criptograth/hash-comparer'
 import { TokenGenerator } from '../../protocols/criptograth/token-generator'
+import { UpdatedAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updatedAccessTokenRepository: UpdatedAccessTokenRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator, updatedAccessTokenRepository: UpdatedAccessTokenRepository) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGenerator = tokenGenerator
+    this.updatedAccessTokenRepository = updatedAccessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -20,6 +23,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updatedAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
     }
