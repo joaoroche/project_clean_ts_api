207033a37ac9b3f433c8c1beaf32137be9d8cd7a
unknown <joaocarlos.roche@globo.com>
2021-02-19 15:30:02 -0300

    refactor: rename Encrypter to Hasher

--- src/data/usecase/add-account/db-add-account-protocols.ts
+++ src/data/usecase/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'
-export * from '../../protocols/criptograth/encrypter'
+export * from '../../protocols/criptograth/hasher'
 export * from '../../protocols/db/add-account-repository'
