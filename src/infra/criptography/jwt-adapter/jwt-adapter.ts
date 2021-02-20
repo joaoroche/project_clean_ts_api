0e06c897563024bf9198efe1535dbc66282ad563
unknown <joaocarlos.roche@globo.com>
2021-02-20 20:55:37 -0300

    Revert "feat: ensure JwtAdapter returns a token when sign succeeds"
    This reverts commit 07b58a036bdaa9254ee80912cf63295c3bc4e040.

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -9,7 +9,7 @@ export class JwtAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    const accessToken = jwt.sign({ id: value }, this.secret)
-    return accessToken
+    jwt.sign({ id: value }, this.secret)
+    return new Promise(resolve => resolve(null))
   }
 }
