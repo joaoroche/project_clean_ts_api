cae567b26ff39f37e90880571e9c67708c6ab3f0
João Carlos <joaocarlos.roche@globo.com>
2021-03-02 21:42:13 -0300

    feat: ensure SignUpController returns 403 if AddAcount return null

---
+++ src/presentation/errors/email-in-use-error.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/quotes */
+export class EmailInUseError extends Error {
+  constructor () {
+    super(`The received email is already in use`)
+    this.name = 'EmailInUseError'
+  }
+}
