14e897ad251e632eaee095b9474299bdb19d4e6d
João Carlos <joaocarlos.roche@globo.com>
2021-03-09 20:51:08 -0300

    refactor: move AuthMiddleware dependencies to a protocols file

---
+++ src/presentation/middlewares/auth-middleware-protocols.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/export */
+export * from '../protocols'
+export * from '../helpers/http/http-helper'
+export * from '../../domain/usecases/load-account-by-token'
+export * from '../../domain/models/account'
