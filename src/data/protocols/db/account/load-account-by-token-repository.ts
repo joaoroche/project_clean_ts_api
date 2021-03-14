57bfbdda79d0ccf6291555ce12d281f5a50b8a46
João Carlos <joaocarlos.roche@globo.com>
2021-03-14 14:13:18 -0300

    feat: ensure AccountMongoRepository find and account by token without role

--- src/data/protocols/db/account/load-account-by-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -2,5 +2,5 @@
 import { AccountModel } from '../../../../domain/models/account'
 
 export interface LoadAccountByTokenRepository {
-  loadByToken (token: string, roke?: string): Promise<AccountModel>
+  loadByToken (token: string, role?: string): Promise<AccountModel>
 }
