041ce42faf86ef49467fc768bd62a26358bbe458
João Carlos <joaocarlos.roche@globo.com>
2021-03-15 21:45:51 -0300

    docs: add ci/cd badges

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!**/**/*protocols.ts*',
+    '!**/**/protocols/index.ts*'
+  ],
   coverageProvider: 'babel',
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
---
+++ readme.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/joaoroche/project_clean_ts_api.svg?branch=master)](https://travis-ci.org/joaoroche/project_clean_ts_api)
+
+[![Coverage Status](https://coveralls.io/repos/github/joaoroche/project_clean_ts_api/badge.svg)](https://coveralls.io/github/joaoroche/project_clean_ts_api)
