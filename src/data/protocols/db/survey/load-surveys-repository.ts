a0f3a3702cc5191b15afba8ca8b65b5e5a5a5e37
João Carlos <joaocarlos.roche@globo.com>
2021-03-17 21:34:25 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/protocols/db/survey/load-surveys-repository.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/method-signature-style */
+
+import { SurveyModel } from '../../../../domain/models/survey'
+
+export interface LoadSurveysRepository {
+  loadAll (): Promise<SurveyModel[]>
+}
