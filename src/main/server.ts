651ee1ddfe633d331d9ad637d10d12e4402b7e75
João Carlos <joaocarlos.roche@globo.com>
2021-03-26 20:43:38 -0300

    fix: make controllers return the error message instead of the error name

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,6 @@
-import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import 'reflect-metadata'
 import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
